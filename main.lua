1e5a77c723ba02c579caa984673f0e37a311d59d
stshing7609 <stshing7609@gmail.com>
2016-03-05 21:19:15 -0500

    Test stuff

--- main.lua
+++ main.lua
@@ -23,6 +23,19 @@ function love.update(dt)
 	test:setX(x)
 	test:setY(y)
 end
+
+local Bullet = require 'TestClass'
+
 function love.draw()
 	love.graphics.draw(test:getImage(), test:getX(), test:getY())
+	love.graphics.draw(yuzu, x, y)
+
+    local a = 5
+    if a == 5 then
+    	love.graphics.print("Blargh", 400, 300)
+    else
+		love.graphics.print("Hello World", 400, 300)
+	end
+
+	Bullet:init(0,0)
 end
