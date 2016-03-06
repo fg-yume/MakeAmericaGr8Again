324504dacd586f2e3df936c37b0b66c14fbd4cc2
ELVIS PEREZ (RIT Student) <eap4247@ad.rit.edu>
2016-03-05 19:09:36 -0500

    added test code for moving sprite (game loop)

--- main.lua
+++ main.lua
@@ -1,3 +1,23 @@
+x = 300
+y = 200
+
+function love.load()
+	yuzu = love.graphics.newImage("yuzu.jpg")
+end
+function love.update(dt)
+	if love.keyboard.isDown("left") then
+		x = x - 60 * dt;
+	end
+	if love.keyboard.isDown("right") then
+		x = x + 60 * dt;
+	end
+	if love.keyboard.isDown("up") then
+		y = y - 60 * dt;
+	end
+	if love.keyboard.isDown("down") then
+		y = y + 60 * dt;
+	end
+end
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+	love.graphics.draw(yuzu, x, y)
 end
