e9a671073481786019690de45aee86b28c32834e
Freddy Garcia <garciaf93@gmail.com>
2016-03-05 19:05:29 -0500

    ATTN: Adds configuration file for the game

---
+++ conf.lua
@@ -0,0 +1,10 @@
+-- Configuration File
+function love.conf(t)
+    t.title = "Make America Great Again"
+    t.version = "0.10.1" -- target LOVE version
+    t.window.width = "800"
+    t.window.height = "500"
+
+    -- Debugging (windows)
+    t.console = true
+end
