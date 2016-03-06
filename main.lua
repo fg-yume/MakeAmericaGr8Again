906044709f22599b0d7b089ffd4d522cd1b6ac47
ELVIS PEREZ (RIT Student) <eap4247@ad.rit.edu>
2016-03-05 21:21:55 -0500

    refactor to try and make the example cleaner (nc)

--- main.lua
+++ main.lua
@@ -1,28 +1,16 @@
 test = require("unit")
-x = 300
-y = 150
+
+test:setX(300)
+test:setY(150)
+test:setSpeed(300)
 
 function love.load()
 	yuzu = love.graphics.newImage("yuzu.jpg")
 	test:setImage(yuzu)
 end
 function love.update(dt)
-	if love.keyboard.isDown("left") then
-		x = x - 60 * dt;
-	end
-	if love.keyboard.isDown("right") then
-		x = x + 60 * dt;
-	end
-	if love.keyboard.isDown("up") then
-		y = y - 60 * dt;
-	end
-	if love.keyboard.isDown("down") then
-		y = y + 60 * dt;
-	end
-	
-	test:setX(x)
-	test:setY(y)
+	test:update(dt)
 end
 function love.draw()
-	love.graphics.draw(test:getImage(), test:getX(), test:getY())
+	test:draw()
 end
--- unit.lua
+++ unit.lua
@@ -12,6 +12,7 @@ function Unit.new()
 	
 	self.x = 0
 	self.y = 0
+	self.speed = 0
 	self.image = nil
 	
 	return self
@@ -33,12 +34,39 @@ function Unit:setY(yValue)
 	self.y = yValue
 end
 
+function Unit:getSpeed()
+	return self.speed
+end
+
+function Unit:setSpeed(speedValue)
+	self.speed = speedValue
+end
+
 function Unit:getImage()
 	return self.image
 end
 
 function Unit:setImage(imageValue)
 	self.image = imageValue
+end
+
+function Unit:update(dt)
+	if love.keyboard.isDown("left") then
+		self.x = self.x - self.speed * dt;
+	end
+	if love.keyboard.isDown("right") then
+		self.x = self.x + self.speed * dt;
+	end
+	if love.keyboard.isDown("up") then
+		self.y = self.y - self.speed * dt;
+	end
+	if love.keyboard.isDown("down") then
+		self.y = self.y + self.speed * dt;
+	end
+end
+
+function Unit:draw()
+	love.graphics.draw(self.image, self.x, self.y)
 end
 
 return Unit
